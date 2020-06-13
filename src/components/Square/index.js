9bf49524da7ad4461a61129be3232524c4faedb6
Artur <artur.prado@gmail.com>
2020-06-12 22:08:25 -0300

    Add doors

--- src/components/Square/index.js
+++ src/components/Square/index.js
@@ -2,10 +2,21 @@ import React, { Component } from 'react';
 import './styles.css';
 
 class Square extends Component  {
+  squareContent = () => {
+    if (this.props.type === 'entrance') {
+      return <i className="fa fa-chevron-up"></i>
+    } else if (this.props.type === 'start')  {
+      return <i className="fa fa-home"></i>
+    } else {
+      return null;
+    }
+  }
+
   render() {
-    const classes = `square ${this.props.state || ''}`
+    const classes = `square ${this.props.state || ''} ${this.props.type}`
     return (
-      <div className={classes} data-row={this.props.row} data-column={this.props.column}>
+      <div className={classes} data-direction={this.props.direction} data-row={this.props.row} data-column={this.props.column}>
+        {this.squareContent()}
       </div>
     )
   }
